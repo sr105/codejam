0ab3c7bd7f4ee6b5142df3e9e6947c77b66efab2
Harvey Chapman <hchapman@3gfp.com>
2015-11-06 14:34:36 -0500

    Cleanup and make debug printing match the example

--- a.bot_trust/bot_trust.py
+++ a.bot_trust/bot_trust.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# https://docs.python.org/3.3/library/string.html#format-string-syntax
+
+debug = False
+
 class Bot:
     def __init__(self, color, test):
         self.color = color
@@ -10,19 +14,16 @@ class Bot:
 
     def move(self, other_bot):
         if not self.has_move():
-            return
+            return 'Stay at button {}'.format(self.position)
         if self.position != self.button:
-            last = self.position
             self.position += 1 if self.position < self.button else -1
-            #print('{}: move from {} to {}'.format(self.color, last, self.position))
-            return
+            return 'Move to button {}'.format(self.position)
         if other_bot.has_move() and other_bot.index < self.index:
             # we have to wait for other bot
-            #print('{}: waiting for {} to press their button'.format(self.color, other_bot.color))
-            return
+            return 'Stay at button {}'.format(self.position)
         # press our button
-        #print('{}: pressing button {}'.format(self.color, self.button))
         self.button_pressed = True
+        return 'Push button {}'.format(self.button)
 
     def end_of_move(self):
         if not self.has_move() or self.button_pressed:
@@ -44,17 +45,20 @@ class Bot:
     
 def run_test(test):
     num_steps,*test = test.split()
-    #print(int(num_steps))
     orange = Bot('O', test)
     blue = Bot('B', test)
     count = 0
+    if debug:
+        print('Time   | Orange             | Blue')
+        print('-------+--------------------+-------------------')
     while True:
         if not orange.has_move() and not blue.has_move():
             break
         count += 1
-        #print('-----'*3,count,'-----'*3)
-        orange.move(blue)
-        blue.move(orange)
+        text1 = orange.move(blue)
+        text2 = blue.move(orange)
+        if debug:
+            print('  {:^3}  | {:18} | {}'.format(count, text1, text2))
         orange.end_of_move()
         blue.end_of_move()
     return count
@@ -62,9 +66,7 @@ def run_test(test):
 def do_test_cases(input_filename):
     with open(input_filename) as input:
         test_case_count = int(input.readline())
-        #print(test_case_count)
         for num, test in enumerate(input):
-            #print(test.strip())
             print ('Case #{}: {}'.format(num + 1, run_test(test)))
 
 if __name__ == '__main__':
